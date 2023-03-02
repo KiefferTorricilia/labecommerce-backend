9bd386450e7c3e0c4d3a9cdefb4e3ee227ba8052
Kieffer Torricilia <kieffer.torricilia@gmail.com>
2023-03-01 22:36:28 -0300

    Exercicio 3 feito

---
+++ src/database.ts
@@ -0,0 +1,44 @@
+import {TUser, TProduct, TPurchase} from './types'
+
+export const users: TUser[] = [
+    {
+        id: '1',
+        email: 'kieffer.torricilia@gmail.com',
+        password: '123423113'
+    },
+    {
+        id: '2',
+        email: 'Arthur@gmail.com',
+        password: '1234123'
+    }
+]
+
+export const products: TProduct[] = [
+    {
+        id: "1",
+        name: "Batata",
+        price: 5,
+        category: "Alimentício"
+    },
+    {
+        id: "2",
+        name: "Cenoura",
+        price: 2,
+        category: "Alimentício"
+    }
+]
+
+export const purchases: TPurchase[] = [
+    {
+        userId: users[0].id,
+        productId: products[0].id,
+        quantity: 10,
+        totalPrice: products[0].price * 10
+    },
+    {
+        userId: users[1].id,
+        productId: products[1].id,
+        quantity: 5,
+        totalPrice: products[0].price * 5
+    }
+]
