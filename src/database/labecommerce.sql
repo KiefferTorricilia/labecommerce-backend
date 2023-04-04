3bc85b7f432c58e8355b6fd67a3a7f76487965b0
kieffer <kieffer.torricilia@gmail.com>
2023-04-03 22:16:26 -0300

    Exercicios de Relações SQL 1 feitos

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,10 +1,10 @@
--- Active: 1679960851720@@127.0.0.1@3306
+-- Active: 1680566558818@@127.0.0.1@3306
 CREATE TABLE 
     users(
         id TEXT PRIMARY KEY UNIQUE NOT NULL,
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
-    );
+);
 
 INSERT INTO users(id, email, password)
 VALUES
@@ -31,3 +31,31 @@ VALUES
     (5, "SSD Sandisk", 219, "Eletrônicos");
 
 DROP TABLE products;
+
+CREATE TABLE purchases(
+    id TEXT UNIQUE NOT NULL PRIMARY KEY,
+    total_price REAL NOT NULL,
+    paid INTEGER NOT NULL,
+    delivered_at TEXT DATETIME,
+    buyer_id TEXT NOT NULL,
+    FOREIGN KEY (buyer_id) REFERENCES users(id)
+);
+
+DROP TABLE purchases;
+
+INSERT INTO purchases(id, total_price, paid, delivered_at, buyer_id)
+VALUES
+    ("1", 20, 1, null, "1"),
+    ("2", 50, 0, datetime('now'), "1"),
+    ("3", 40, 1, null, "2"),
+    ("4", 80, 0, datetime('now'), "2");
+
+UPDATE purchases SET delivered_at = datetime('now') WHERE id = 1;
+UPDATE purchases SET delivered_at = datetime('now') WHERE id = 3;
+
+SELECT * FROM purchases;
+
+SELECT * FROM purchases
+INNER JOIN users
+ON purchases.buyer_id = users.id;
+
